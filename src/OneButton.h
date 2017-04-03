a3cbedcea785046682658e515fc0fcd7b341fe74
supermarioli <supermarioli@gmail.com>
2017-04-03 17:02:17 +0200

    simple update needs to be tested.
    simple update needs to be tested.

--- src/OneButton.h
+++ src/OneButton.h
@@ -20,7 +20,8 @@
 // ----- Callback function types -----
 
 extern "C" {
-  typedef void (*callbackFunction)(void);
+  // typedef void (*callbackFunction)(void);
+  typedef std::function<void()> callbackFunction;
 }
 
 
@@ -51,6 +52,7 @@ public:
   // call this function every some milliseconds for handling button events.
   void tick(void);
   bool isLongPressed();
+  bool isPressed();
 
 private:
   int _pin;        // hardware pin number. 
